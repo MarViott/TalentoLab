86f3daf7bab9b36c0b317de97b51c775aebec328
marisa <marisaviotti@gmail.com>
2025-10-06 12:50:38 -0300

    feat: Limpiar código eliminando líneas en blanco innecesarias en varios componentes

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './styles/Inicio.css';
 
-
 const Inicio = () => {
     return (
         <>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 // src/components/LoginButton.jsx
+import React from 'react';
 import { loginWithGoogle } from '../api/google';
-
 export default function LoginButton() {
   return <button onClick={loginWithGoogle}>Iniciar sesión con Google</button>;
 }
+
