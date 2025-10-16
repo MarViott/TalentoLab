2f9df2273b85e71232075ecc7c77766a03cfc1f9
marisa <marisaviotti@gmail.com>
2025-10-16 16:37:22 -0300

    feat: Agregar componentes de autenticación y rutas protegidas, incluyendo IniciarSesion, Pagar y RutaProtegida

--- src/components/Login.jsx
+++
@@ -1,7 +0,0 @@
-// src/components/LoginButton.jsx
-import React from 'react';
-import { loginWithGoogle } from '../api/google';
-export default function LoginButton() {
-  return <button onClick={loginWithGoogle}>Iniciar sesión con Google</button>;
-}
-
