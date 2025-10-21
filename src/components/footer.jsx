2e702bfd99a93fcd056de451d880b3bbf4e97868
marisa <marisaviotti@gmail.com>
2025-10-21 13:58:49 -0300

    footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,13 +1,26 @@
 import React from 'react';  
 function Footer() {  
-    return (  
-        <footer style={{ 
-            backgroundColor: "#f1f1f1", 
-            padding: "10px", 
-            textAlign: "center", 
-            marginTop: "20px" }}>  
-            <p>&copy; 2025 - Talento Lab</p>  
-        </footer>  
+    return (
+      <footer
+        style={{
+          backgroundColor: "#f1f1f1",
+          padding: "10px",
+          textAlign: "center",
+          marginTop: "20px",
+        }}
+      >
+        <p>
+          &copy; 2025 - Desarrollado por  
+       <a
+            href="https://www.linkedin.com/in/marisa-viotti-74601036a/"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "#0077b5", textDecoration: "none" }}
+          >
+            Marisa Viotti
+          </a>
+           </p>
+      </footer>
     );  
 }  
 
