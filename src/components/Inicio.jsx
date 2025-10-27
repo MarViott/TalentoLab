f58a8adf1af8ab754f90a311d89ffc082d1fe3bf
marisa <marisaviotti@gmail.com>
2025-10-27 14:38:08 -0300

    video de inicio

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -6,7 +6,7 @@ const Inicio = () => {
     return (
         <>
         <div className="inicio-container">
-            <h1 style={{padding: "20px 300px"}}>
+            <h1 style={{padding: "10px 300px"}}>
                 ¡Bienvenido!
             </h1>
             <b><p style={{color: "#666" }}>Aquí comienza tu proyecto tecnológico!</p></b>
